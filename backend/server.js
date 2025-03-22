e8f24c27a5089b412b4f0bcbb7d5497160dc7245
FalcoWolff <Falco_W2004@web.de>
2025-03-22 13:54:43 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,125 @@
+import WebSocket, {WebSocketServer} from 'ws';
+import express from 'express';
+import { spawn } from 'child_process';
+import { mkdirSync } from 'fs';
+import { storeGraph } from './util/GraphHelper.js';
+import path from 'path';
+
+const cProgramPath = "/home/falco/coding/vscode/projects/Graph-homomorphisms/cmake-build-release/Graph_homomorphisms";
+
+const expressPort = 3000;
+const websocketPort = 3001;
+
+const wss = new WebSocketServer({ port: websocketPort });
+const wsClients = [];//store the active ws clients
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  wsClients.push(ws)
+
+  ws.on('message', (message) => {
+    console.log('Received:', message);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    wsClients = wsClients.clients.filter(client => client !== ws);
+  });
+});
+
+console.log(`WebSocket server running on ws://localhost:${websocketPort}`);
+
+const app = express();
+
+// Middleware to handle WebSocket upgrade
+app.server = app.listen(expressPort, () => {
+    console.log(`REST API server running on http://localhost:${expressPort}`);
+});
+
+app.server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request);
+    });
+});
+
+app.use(express.json()); //to parse JSON bodies
+
+let nextTaskId = 0;
+
+app.post('/createTask', (req, res) => {
+    let taskId = nextTaskId;
+    nextTaskId++;
+    console.log('Received task create request ' + taskId);
+
+    const data = req.body;
+    console.log(data);
+    const type = data.type;
+    console.log("type: " + type);
+
+    const taskFolder = "./tasks/task_" + taskId
+    mkdirSync(taskFolder);
+
+    let params = [];
+
+    switch(type) {
+        case "hom":
+            const G = data.G;
+            const H = data.H;
+            const Gfile = path.resolve(taskFolder + "/Graph_G.txt");
+            const Hfile = path.resolve(taskFolder + "/Graph_H.txt");
+            storeGraph(Gfile, G);
+            storeGraph(Hfile, H);
+
+            params.push("hom")
+            params.push(Gfile)
+            params.push(Hfile)
+    }
+
+    // Start the task (spawning the C program)
+    const cProgram = spawn(cProgramPath, params);
+    let output = '';
+
+    cProgram.stdout.on('data', (data) => {
+        output += data.toString(); // Capture the output
+    });
+
+    cProgram.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    cProgram.on('close', (code) => {
+        console.log(`C program finished with code ${code} and output: ${output}`);
+
+        const message = {
+            taskId,
+            taskStatus: 'completed',
+            output: output,
+            exitCode: code,
+        };
+
+        //notify all clients about the task completion
+        wsClients.forEach((client) => {
+            client.send(JSON.stringify(message));
+        });
+    });
+
+    cProgram.on('error', (err) => {
+        console.error(`Error: ${err.message}`);
+
+        //notify all clients about the error
+        const errorMessage = {
+            taskId,
+            taskStatus: 'error',
+            error: err.message,
+        };
+
+        wsClients.forEach((client) => {
+            client.send(JSON.stringify(errorMessage));
+        });
+    });
+
+    res.json({
+        taskId,
+        taskStatus: 'in-progress',
+    });
+});
