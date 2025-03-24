f9dd860f7461914e2e0b5449da3c8dd70733969d
FalcoWolff <Falco_W2004@web.de>
2025-03-24 15:52:06 +0100

    feat: add random graph on the homepage

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -3,11 +3,18 @@ import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 import { useNavigate } from "react-router";
 import { useDispatch } from "react-redux";
 import { insertTask } from "../../store/tasksSlice";
+import uuid from "react-uuid";
+import randomIntFromInterval from "../../util/randomIntFromInterval";
+import { useEffect, useState } from "react";
+import Graph from "react-graph-vis"
 
 export default function Home({}) {
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
+
+    let [graphKey, setGraphKey] = useState(uuid())
+    const [graph, setGraph] = useState({nodes: [], edges: []})
 
     function onClickGetStarted() {
 
@@ -23,12 +30,61 @@ export default function Home({}) {
         navigate("/task/0")
     }
 
+    function generateRandomGraph() {
+        const nodes = []
+        const edges = []
+
+        const n = randomIntFromInterval(20, 100);
+        const m = randomIntFromInterval(n, 2.5*n);
+
+        for(let i = 0; i < n; i++) {
+            nodes.push({id: i, label: i});
+        }
+
+        for(let i = 0; i < m; i++) {
+            const v1 = randomIntFromInterval(0, n-1);
+            const v2 = randomIntFromInterval(0, n-1);
+
+            if(v1 == v2) {
+                i--;
+                continue;
+            }
+
+            edges.push({from: v1, to: v2})
+        }
+
+        setGraph({nodes, edges})
+        setGraphKey(uuid())
+    }
+
+    useEffect(() => {
+        generateRandomGraph();
+    }, [])
+
+    const options = {
+        autoResize: true,
+        layout: {
+            hierarchical: false,
+        },
+        edges: {
+            smooth: true,
+        },
+        height: '800px',
+        interaction: {
+            zoomView: false
+        }
+
+    };
+
     return (
-    <Box sx={{display: 'flex', justifyContent: 'center', marginTop: 10}}>
-        <Box>
-            <Typography variant="h2">Welcome to Graph-homs-web!</Typography>
-            <Button size="large" variant="contained" endIcon={<DoubleArrowIcon/>} onClick={onClickGetStarted}>Get started</Button>
-        </Box>
-    </Box>
+        <div style={{position: 'relative', height: '90vh'}}>
+            <Box sx={{display: 'flex', justifyContent: 'center', zIndex: 5, position: 'relative', marginTop: 8}}>
+                <Box>
+                    <Typography variant="h2">Welcome to Graph-homs-web!</Typography>
+                    <Button size="large" variant="contained" endIcon={<DoubleArrowIcon/>} onClick={onClickGetStarted}>Get started</Button>
+                </Box>
+            </Box>
+            <Graph graph={graph} options={options} key={graphKey}/>
+        </div>
     )
 }
