8062dc7bc828d658b32e96bb22345fcd4bb414b3
ChristophGreger <mail@christoph-greger.de>
2025-03-27 22:36:25 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { storeGraph } from './util/GraphHelper.js';
 import path from 'path';
 import cors from 'cors'
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
